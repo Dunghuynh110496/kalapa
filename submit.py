dc45fdeb90c3fa600505e03b3f4617f1b48eef68
Dunghuynh110496 <dunghuynh110496@gmail.com>
2020-07-16 23:45:49 +1000

    say st

--- submit.py
+++ submit.py
@@ -1,40 +1,8 @@
-import pandas as pd
-import wandb
-from sklearn import metrics
-from sklearn.ensemble import GradientBoostingClassifier
+import os
 
-wandb.init( project = "kalapa")
-config = {
-    "data_version" : "12-07-2020-test",
-    "code_version" : "",
-    "weight_version" : ""
-}
+data_version = "16-07-2020"
+code_version = os.popen('git rev-parse HEAD').read().strip()
+weight_version = "abcasdasdasd"
 
-
-df_train = pd.read_csv(f"../../data/kalapa/{config['data_version']}/train.csv")
-df_dev = pd.read_csv(f"../../data/kalapa/{config['data_version']}/dev.csv")
-test = pd.read_csv(f"../../data/kalapa/{config['data_version']}/test.csv")
-
-X_train = df_train.iloc[:,1:]
-y_train = df_train["label"]
-
-X_dev = df_dev.iloc[:,1:]
-y_dev = df_dev["label"]
-
-model_xgb = GradientBoostingClassifier(random_state = 50)
-model_xgb.fit(X_train, y_train)
-predictions_xgb = model_xgb.predict_proba(X_dev)
-predictions_xgb = predictions_xgb[:,1]
-
-fpr, tpr, thresholds = metrics.roc_curve(y_dev, predictions_xgb, pos_label=1)
-auc_xgb = metrics.auc(fpr, tpr)
-gini_xgb = 2*auc_xgb - 1
-
-config['gini'] = gini_xgb
-wandb.log(config)
-
-predictions_xgb = model_xgb.predict_proba(test)
-predictions_xgb = predictions_xgb[:,1]
-
-print(predictions_xgb)
+os.system(f"python run.py -d {data_version} -c {code_version} -w {weight_version}")
 
