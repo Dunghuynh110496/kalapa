2ca4c21bdec66c7ec198d8b8ac71953f17a3235d
Dunghuynh110496 <dunghuynh110496@gmail.com>
2020-07-18 22:15:09 +1000

    test

--- submit.py
+++
@@ -1,7 +0,0 @@
-import os
-
-data_version = "17-07-2020"
-code_version = os.popen('git rev-parse HEAD').read().strip()
-weight_version = "abcasdasdasd"
-
-os.system(f"python run.py -d {data_version} -c {code_version} -w {weight_version}")
