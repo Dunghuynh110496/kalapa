1820908e129ebb1156560ccbcbd2e6e1104f7964
Dunghuynh110496 <dunghuynh110496@gmail.com>
2020-07-30 17:34:58 +1000

    test

--- run.py
+++ run.py
@@ -70,7 +70,7 @@ def main(args):
             wandb.log(log)
     clf = lgb.train(params,
               d_train,
-              7500,
+              10000,
             callbacks=[evaluate])
     dev["pred"] = best_dev_pred
     test["label"] = best_test_pred
