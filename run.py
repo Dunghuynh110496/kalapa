fa2fe19c397b0a0fb80027e4a013de452ee8fed3
Dunghuynh110496 <dunghuynh110496@gmail.com>
2020-07-31 19:09:55 +1000

    test

--- run.py
+++ run.py
@@ -79,7 +79,7 @@ def main(args):
             d_train = lgb.Dataset(train, label=train_dev.iloc[train_index]["label"])
             clf = lgb.train(params,
                       d_train,
-                      1000,
+                      10000,
                     callbacks=[evaluate])
             ginis.append(best_gini)
             pred_test.append(best_test_pred)
