ff8ac199495fbd5337a50ca2a7621c05ed8b2bc0
Dunghuynh110496 <dunghuynh110496@gmail.com>
2020-08-02 22:17:17 +1000

    test

--- run.py
+++ run.py
@@ -35,9 +35,7 @@ def main(args):
             if df_fe[col].dtype.name == "object":
                 df_fe[col] = df_fe[col].astype('category')
             vc = df_fe[col].value_counts()
-            if len(vc) <= 2:
-                df_fe[col] = df_fe[col].astype('category')
-        return df_fe
+
     train = cate(train)
     test = cate(test)
 
