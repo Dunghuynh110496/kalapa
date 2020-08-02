765bb1066aacfce15314a8c109c53b01e99d5520
Dunghuynh110496 <dunghuynh110496@gmail.com>
2020-08-02 21:17:27 +1000

    test

--- run.py
+++ run.py
@@ -33,7 +33,7 @@ def main(args):
     def cate(df_fe):
         for col in df_fe.columns:
             if df_fe[col].dtype.name == "object":
-                df_fe[col] = df_train[col].astype('category')
+                df_fe[col] = df_fe[col].astype('category')
         return df_fe
     train = cate(train)
     test = cate(test)
