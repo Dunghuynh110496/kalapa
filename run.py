0d121bc32bc5ef0b621e8dc4623a7a2693a6d6f9
Dunghuynh110496 <dunghuynh110496@gmail.com>
2020-08-07 16:36:06 +1000

    test

--- run.py
+++ run.py
@@ -7,6 +7,7 @@ import numpy as np
 
 from sklearn.metrics import roc_auc_score
 from sklearn.model_selection import StratifiedKFold
+from sklearn.ensemble import RandomForestClassifier
 
 def gini(y_true, y_score):
     return roc_auc_score(y_true, y_score)*2 - 1
