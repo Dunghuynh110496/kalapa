5004a54d5f3573898278e4093c1d434cd255307c
Dunghuynh110496 <dunghuynh110496@gmail.com>
2020-08-07 21:40:58 +1000

    test

--- run.py
+++ run.py
@@ -60,7 +60,7 @@ def main(args):
             print(X_train.info())
             y_dev = y_label.iloc[dev_idx]
             X_test = test_fe.iloc[:,1:]
-            clf = RandomForestClassifier(n_estimators=1)
+            clf = RandomForestClassifier()
             clf.fit(X_train, y_train)
             #output =  [test_preds, train_gini, dev_gini]
             output = evaluate(i,clf, X_train, y_train, X_dev, y_dev, X_test)
