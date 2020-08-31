870df68c5cde193764efbb2c43c61b273a607015
Dunghuynh110496 <dunghuynh110496@gmail.com>
2020-08-31 16:01:02 +1000

    test

--- run.py
+++ run.py
@@ -44,8 +44,8 @@ def main(args):
                "weight_version": args.weight_version})
     train = pd.read_csv(f"../../data/kalapa/{args.data_version}/train.csv")
     test = pd.read_csv(f"../../data/kalapa/{args.data_version}/test.csv")
-    train = train.fillna(0)
-    test = test.fillna(0)
+    train = train.fillna(-9999999999)
+    test = test.fillna(-9999999999)
 
     def kfold(train_fe, test_fe):
         y_label = train_fe.label
